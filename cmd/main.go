34663353dc62c3171a2861a27b3f798f07c05c00
Light <danil.odinzov181@gmail.com>
2025-05-13 03:45:06 +0500

    Рефаторинг:
    - Логика инициализации и запуска приложения вынесена в internal/app/app.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,98 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	confloader "messenger/internal/config/loaders"
-	confrpov "messenger/internal/config/providers"
-
-	processor "messenger/internal/messaging/processor"
-	receiver "messenger/internal/messaging/receiver"
-	sender "messenger/internal/messaging/sender"
-
-	ws "messenger/internal/ws"
-	wsh "messenger/internal/ws/handlers"
-	wsupgr "messenger/internal/ws/upgraders"
-
-	"github.com/spf13/viper"
+	app "messenger/internal/app"
 )
 
-// main — это точка входа в приложение. Она инициализирует конфигурацию,
-// настраивает WebSocket-сервис и запускает WebSocket-сервер.
-//
-// Функция выполняет следующие шаги:
-//  1. Загружает конфигурационный файл из указанного пути, с заданным именем и типом файла.
-//  2. Обрабатывает возможные ошибки при загрузке конфигурации, включая ошибки пути,
-//     отсутствие конфигурационного файла и ошибки разбора.
-//  3. Извлекает значения конфигурации, связанные с WebSocket, такие как хост, порт и режим отладки.
-//  4. Инициализирует компоненты для отправки, получения и обработки WebSocket-сообщений.
-//  5. Создает WebSocket-сервис, используя инициализированные компоненты и значения конфигурации.
-//  6. Запускает WebSocket-сервер на указанном хосте и порту.
+// main — это точка входа в приложение.
 func main() {
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		configPath = "../configs"
-		log.Printf("CONFIG_PATH не задан, используется путь по умолчанию: %s", configPath)
-	}
-
-	configFilename := "config"
-	configFiletype := "yaml"
-
-	viperConfigProvider := &confrpov.ViperConfigProvider{}
-
-	config, err := confloader.LoadConfig(viperConfigProvider, configPath, configFilename, configFiletype)
-
-	if err != nil {
-		switch e := err.(type) {
-		case *os.PathError:
-			log.Fatalf("Ошибка пути: %v", e)
-		case *viper.ConfigFileNotFoundError:
-			log.Fatalf("Конфигурационный файл не найден: %v", e)
-		case *viper.ConfigParseError:
-			log.Fatalf("Ошибка при разборе конфигурации: %v", e)
-		default:
-			log.Fatalf("Неизвестная ошибка: %v", e)
-		}
-	}
-
-	certificate, err := confloader.LoadCertificate(config.Certificate)
-	if err != nil {
-		log.Fatalf("Ошибка загрузки сертификата: %v", err)
-	}
-
-	wsHost, wsPort, wsDebug, invalidOrigins := confloader.LoadWebsocket(config.WebSocket)
-
-	wsUpgrager := wsupgr.NewUpgrader(wsDebug, invalidOrigins)
-
-	wsHandler := wsh.NewWebSocketHandler(
-		wsUpgrager,
-		sender.NewWebSocketMessageSender(),
-		receiver.NewWebSocketMessageReceiver(),
-		processor.NewWebSocketMessageProcessor(),
-	)
-
-	wsHandlerFunc := http.HandlerFunc(wsHandler.HandleWebSocket)
-
-	address := fmt.Sprintf("%s:%s", wsHost, wsPort)
-
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{certificate},
-		MinVersion:   tls.VersionTLS12,
-	}
-
-	httpServer := &http.Server{
-		Addr:      address,
-		Handler:   wsHandlerFunc,
-		TLSConfig: tlsConfig,
-	}
-
-	wsService := ws.NewWebsocketService(
-		httpServer,
-	)
-
-	wsService.StartServer()
+	app.Run()
 }
---
+++ internal/app/app.go
@@ -0,0 +1,108 @@
+package app
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	confloader "messenger/internal/config/loaders"
+	confrpov "messenger/internal/config/providers"
+
+	processor "messenger/internal/messaging/processor"
+	receiver "messenger/internal/messaging/receiver"
+	sender "messenger/internal/messaging/sender"
+
+	ws "messenger/internal/ws"
+	wsh "messenger/internal/ws/handlers"
+	wsupgr "messenger/internal/ws/upgraders"
+
+	"github.com/spf13/viper"
+)
+
+// Run инициализирует и запускает приложение WebSocket-сервера.
+// Выполняются следующие шаги:
+// 1. Загружается конфигурационный файл с использованием ViperConfigProvider.
+//   - Если переменная окружения CONFIG_PATH не задана, используется путь по умолчанию.
+//
+// 2. Обрабатываются возможные ошибки при загрузке конфигурации, включая:
+//   - Ошибки пути
+//   - Отсутствие конфигурационного файла
+//   - Ошибки разбора конфигурации
+//
+// 3. Загружается TLS-сертификат для безопасной связи.
+// 4. Настраиваются параметры WebSocket, включая хост, порт, режим отладки и недопустимые источники.
+// 5. Создается и инициализируется обработчик WebSocket с необходимыми компонентами:
+//   - Upgrader для WebSocket-соединений
+//   - Отправитель, получатель и обработчик сообщений.
+//
+// 6. Настраивается HTTP-сервер с конфигурацией TLS и обработчиком WebSocket.
+// 7. Запускается WebSocket-сервер.
+//
+// Эта функция регистрирует фатальные ошибки и завершает приложение, если возникают
+// критические проблемы во время инициализации или запуска.
+func Run() {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configPath = "../configs"
+		log.Printf("CONFIG_PATH не задан, используется путь по умолчанию: %s", configPath)
+	}
+
+	configFilename := "config"
+	configFiletype := "yaml"
+
+	viperConfigProvider := &confrpov.ViperConfigProvider{}
+
+	config, err := confloader.LoadConfig(viperConfigProvider, configPath, configFilename, configFiletype)
+
+	if err != nil {
+		switch e := err.(type) {
+		case *os.PathError:
+			log.Fatalf("Ошибка пути: %v", e)
+		case *viper.ConfigFileNotFoundError:
+			log.Fatalf("Конфигурационный файл не найден: %v", e)
+		case *viper.ConfigParseError:
+			log.Fatalf("Ошибка при разборе конфигурации: %v", e)
+		default:
+			log.Fatalf("Неизвестная ошибка: %v", e)
+		}
+	}
+
+	certificate, err := confloader.LoadCertificate(config.Certificate)
+	if err != nil {
+		log.Fatalf("Ошибка загрузки сертификата: %v", err)
+	}
+
+	wsHost, wsPort, wsDebug, invalidOrigins := confloader.LoadWebsocket(config.WebSocket)
+
+	wsUpgrager := wsupgr.NewUpgrader(wsDebug, invalidOrigins)
+
+	wsHandler := wsh.NewWebSocketHandler(
+		wsUpgrager,
+		sender.NewWebSocketMessageSender(),
+		receiver.NewWebSocketMessageReceiver(),
+		processor.NewWebSocketMessageProcessor(),
+	)
+
+	wsHandlerFunc := http.HandlerFunc(wsHandler.HandleWebSocket)
+
+	address := fmt.Sprintf("%s:%s", wsHost, wsPort)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	httpServer := &http.Server{
+		Addr:      address,
+		Handler:   wsHandlerFunc,
+		TLSConfig: tlsConfig,
+	}
+
+	wsService := ws.NewWebsocketService(
+		httpServer,
+	)
+
+	wsService.StartServer()
+}
