7e7c421ab9e8fe76af71feb039e9e6dd6a7793e7
Light <danil.odinzov181@gmail.com>
2025-05-16 03:05:58 +0500

    Документирование:
    - Добавление документации для функции New для WebSocketHandlerFactory

--- internal/factories/wshandler.go
+++ internal/factories/wshandler.go
@@ -17,6 +17,20 @@ type WebSocketHandlerFactory struct {
 	processorOptions processor.Options
 }
 
+// New создает и возвращает новый экземпляр WebSocketHandlerFactory с предоставленными
+// параметрами upgrader, senderOptions, receiverOptions и processorOpts.
+// Инициализирует фабрику необходимыми зависимостями для обработки WebSocket-соединений.
+//
+// Параметры:
+//
+//   - upgrader        - websocket.Upgrader для апгрейда HTTP-соединений до WebSocket.
+//   - senderOptions   - Опции конфигурации для компонента отправки сообщений.
+//   - receiverOptions - Опции конфигурации для компонента приема сообщений.
+//   - processorOpts   - Опции конфигурации для компонента обработки сообщений.
+//
+// Возвращает:
+//
+//   - *WebSocketHandlerFactory - Указатель на инициализированную фабрику.
 func New(
 	upgrader websocket.Upgrader,
 	senderOptions sender.Options,
@@ -31,6 +45,9 @@ func New(
 	}
 }
 
+// NewHandler создает и возвращает новый экземпляр handlers.WebSocketHandler,
+// инициализируя его настроенным upgrader, sender, receiver и processor.
+// Зависимости создаются с использованием опций фабрики.
 func (f *WebSocketHandlerFactory) NewHandler() *handlers.WebSocketHandler {
 	return handlers.New(
 		f.upgrader,
